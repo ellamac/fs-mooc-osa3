e0317b0f55fdb5b76dbc65d658b0a682ebd427ce
Ella Mäkelä <ella@makela.co>
2019-12-26 19:34:59 +0200

    frontti build

---
+++ build/precache-manifest.d9feddfc6e62c9d05937d40e710fc031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cf470afc4b12a15dbe41c50c0b3f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b15a1dc55e13301be1",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "332e94772b7fef02148c",
+    "url": "/static/js/2.923ab7fc.chunk.js"
+  },
+  {
+    "revision": "30b15a1dc55e13301be1",
+    "url": "/static/js/main.88642412.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(bodyParser.json());
 
